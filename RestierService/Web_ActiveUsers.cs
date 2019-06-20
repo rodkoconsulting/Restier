c57376ea9e24cdd4c5b43ef42b7d8477040506b2
Andrew Rodko <arodko@polanerselections.com>
2019-06-20 15:50:48 -0400

    Revert "Remove Key decorator from all fields except for UserName"
    This reverts commit b2bc7bab2601e14675801b85fc83f250c6324722.

--- RestierService/Web_ActiveUsers.cs
+++ RestierService/Web_ActiveUsers.cs
@@ -13,15 +13,21 @@ namespace RestierService
         [StringLength(25)]
         public string UserName { get; set; }
 
+        [Key]
+        [Column(Order = 1)]
         [StringLength(10)]
         public string AccountType { get; set; }
 
         [StringLength(4)]
         public string RepCode { get; set; }
 
+        [Key]
+        [Column(Order = 2)]
         [StringLength(2)]
         public string State { get; set; }
 
+        [Key]
+        [Column(Order = 3)]
         public decimal ZoomPercent { get; set; }
     }
 }
