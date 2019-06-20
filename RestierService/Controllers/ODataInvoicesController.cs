3188a4cc2318535321d1898a75c817cadda4dc80
Andrew Rodko <arodko@polanerselections.com>
2019-06-20 17:31:12 -0400

    Return empty data set instead of null if user not valid

---
+++ RestierService/Controllers/ODataInvoicesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.OData;
+using RestierService;
+
+namespace RestierService.Controllers
+{
+    public class ODataInvoicesController : ODataController
+    {
+        private PolModel db = new PolModel();
+
+        [EnableQuery]
+        public IQueryable<ODataInvoices> Get()
+        {
+            var user = Helper.GetUser();
+            if (user == null) return Enumerable.Empty<ODataInvoices>().AsQueryable();
+            //return Helper.IsManager(user) ? db.ODataInvoices : db.ODataInvoices.Where(u => u.InvR == user.RepCode || u.AcctR == user.RepCode);
+            return db.ODataInvoices;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
