d7f9dac99db1cd606aaedb5397e8d32bd150d856
Gleb Karapish <gleb@altarey.com>
2022-08-17 17:13:03 -0400

    Removed unused usings

--- RestierServiceV2/ODataOrders.cs
+++ RestierServiceV2/ODataOrders.cs
@@ -1,12 +1,8 @@
-using System.Runtime.InteropServices;
-
 namespace RestierServiceV2
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     public partial class ODataOrders
     {
